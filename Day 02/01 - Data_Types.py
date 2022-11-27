1612af81e6657906a73cbedfe124b636828556a5
Mounik <mounicou@gmail.com>
2022-11-27 13:19:21 +0100

    Premier exercice du jour 2

---
+++ Day 02/01 - Data_Types.py
@@ -0,0 +1,10 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+one = int(two_digit_number[0])
+two = int(two_digit_number[1])
+#print(f"{two_digit_number[0]} + {two_digit_number[1]} = {one + two}")
+print(one + two)
---
+++ Day 02/Data_Types.py
@@ -0,0 +1,34 @@
+#Data Types
+
+# String
+
+print("Hello"[-1])
+
+print("123" + "345")
+
+# Integer
+
+print(123 + 345)
+
+print(1_269_035)
+
+# Float
+
+print(3.14159)
+
+# Boolean
+
+True
+False
+
+# Conversion
+# On stocke le nom de l'utilisateur
+num_char = len(input("What is your name?\n"))
+# On le converti en string
+new_num_char = str(num_char)
+# On imprime le résultat
+print("Your name has " + new_num_char + " characters.")
+
+# Plus rapide le f-string
+num_char = len(input("What is your name?\n"))
+print(f"Your name has {num_char} characters.")
