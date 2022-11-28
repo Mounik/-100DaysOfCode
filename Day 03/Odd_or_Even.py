4e8edba21e74da720318b042f059837822c16d0f
Mounik <mounicou@gmail.com>
2022-11-28 21:31:18 +0100

    La solution du cours plus simple que moi ^^

--- Day 03/Odd_or_Even.py
+++ Day 03/Odd_or_Even.py
@@ -4,9 +4,7 @@ number = int(input("Which number do you want to check ? "))
 
 # Write your code below this line 👇
 
-modulo = number % 2
-
-if modulo == 0:
+if number % 2 == 0:
     print("This is an even number.")
 else:
     print("This is an odd number.")
