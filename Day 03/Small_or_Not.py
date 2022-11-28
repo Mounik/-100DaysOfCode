f5ff74470a2d0aa238baafa041e9008defece7b0
Mounik <mounicou@gmail.com>
2022-11-28 21:20:34 +0100

    Fin du premier exo et début du deuxième de la journée

---
+++ Day 03/Small_or_Not.py
@@ -0,0 +1,11 @@
+# 🚨 Don't change the code below 👇
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm ? "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+if height >= 120:
+    print("You can ride the rollercoaster")
+else:
+    print("Sorry, you have to grow taller before you can ride")
