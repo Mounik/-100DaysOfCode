6b364edcd2ea54c30db909403bcb4a1d7583ad65
Mounik <mounicou@gmail.com>
2022-11-28 22:29:26 +0100

    Corection de l'imbrication if else

---
+++ Day 03/Leap_Year.py
@@ -0,0 +1,27 @@
+# 🚨 Don't change the code below 👇
+year = int(input("Which year do you want to check? "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+if year % 4 == 0:
+    print("Leap year.")
+    if year % 100 == 0:
+        print("Not leap year.")
+        if year % 400 == 0:
+            print("Leap year.")
+        else:
+            print("Not leap year.")
+    else:
+        print("Leap year.")
+else:
+    print("Not leap year.")
+
+# if year % 4 == 0:
+#     print("Leap year.")
+# elif year % 100 == 0:
+#         print("Not leap year.")
+# elif year % 400 == 0:
+#             print("Leap year.")
+# else:
+#     print("Not leap year.")
